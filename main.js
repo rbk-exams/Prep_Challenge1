5adaf3accca4c4295919481ac9a9a9eb8dd64df8
SaharAl-shurafat1990 <hayelsahar@gmail.com>
2019-03-08 23:08:32 +0200

    init

---
+++ main.js
@@ -0,0 +1,76 @@
+//======================================================================= 1
+//Write a function to greet a person. Function will take name as input and greet the person by saying hello. Return null if input is empty string or null.
+//greet("kitty") === "hello Kitty!"
+//greet("") === null; // Return null if input is empty string
+//greet(null) === null; // Return null if input is null.
+function greet(name) {
+  // TODO: your code here  
+} 
+//=======================================================================2
+// A restuarnt adds to the original cost of any bill:
+// 8 % tax 
+// 20 % tip
+// write a function called billTotal that takes the bill cost as an input and returns the total bill amount (including the tax and the tips) as an output
+
+function billTotal(subtotal) {
+ // your code is here
+}
+
+//=======================================================================3
+// write a function called multiplyTwo that takes an array of numbers as an input,
+// and returns a new array with all the array elements multiplied by two as an output
+// solve this question using while loop. 
+// multiplyTwo( [1,2,3,4] ) ==> [2,4,6,8]
+// multiplyTwo( [3,6,9] ) ==> [6,12,18]
+ 
+function multiplyTwo(array) {
+  // your code is here
+}
+//=======================================================================4
+//Write a function countLetters that, when given a string as an argument, 
+//returns an object where keys are the letters in the string, 
+//and values are the number of occurrences of that letter within the string:
+
+ function countLetters(s) { 
+    // your code is here
+ } 
+ //countLetters('hii'); // => {h: 1, i: 2}
+ //countLetters('ahh'); // => {a: 1, h: 2} 
+
+//=======================================================================5
+//Write a function named getMiddle that returns the value of the middle element in an array. 
+//If the array has an even number of elements, then this function must return the average of the two middle elements.
+function getMiddle(array) {
+  // your code is here
+}
+      
+//=======================================================================6
+//Using recursion, return the sum of all of the positives numbers of an array of numbers.
+// posSum[1,-4,7,12] => 1 + 7 + 12 = 20
+ 
+function posSum(arr) {
+  // TODO: your code here          
+}
+
+//=======================================================================7
+/*
+Write a function that uses console.log to give the following triangle:
+#
+## 
+###
+####
+#####
+######
+#######
+*/
+
+function drawTriangle() {
+    // TODO: your code here
+}
+ 
+// Good luck :)
+
+
+
+
+ 
